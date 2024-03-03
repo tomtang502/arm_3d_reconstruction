a46afdf58afb6bdeb8164662b959b6f3cb888b87
Tom <hz20020801@gmail.com>
2024-03-02 23:29:44 -0500

    update conda dependencies

--- cam_arm_traj/utils/arm_motion.py
+++ cam_arm_traj/utils/arm_motion.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../lib")
+sys.path.append("../../z1_sdk/lib")
 import unitree_arm_interface
 import time
 import numpy as np
@@ -13,17 +13,14 @@ arm.setFsmLowcmd()
 
 duration = 1000
 lastPos = arm.lowstate.getQ()
-print("init", lastPos)
 targetPos = np.array([0.0, 1.5, -1.0, -0.54, 0.0, 0.0]) #forward
-cur_cart_pos = unitree_arm_interface.Z1Model.forwardKinematics(lastPos)
 
 for i in range(0, duration):
     arm.q = lastPos*(1-i/duration) + targetPos*(i/duration)# set position
     arm.qd = (targetPos-lastPos)/(duration*0.002) # set velocity
     arm.tau = armModel.inverseDynamics(arm.q, arm.qd, np.zeros(6), np.zeros(6)) # set torque
     arm.gripperQ = -1*(i/duration)
-    #print(arm.q)
-    
+
     arm.setArmCmd(arm.q, arm.qd, arm.tau)
     arm.setGripperCmd(arm.gripperQ, arm.gripperQd, arm.gripperTau)
     arm.sendRecv()# udp connection
