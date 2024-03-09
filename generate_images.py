f2803bfba10e8bcdc097043788d32e91d57622c8
Tom <hz20020801@gmail.com>
2024-03-08 22:52:52 -0500

    new images with apriltag in it

--- generate_images.py
+++ generate_images.py
@@ -11,9 +11,9 @@ list of experiment tags and their saved image folder names (same means same name
 2back_2sidesym_leftback3linearx : 2b2sslb3_block (6+2 images) 
 2backsym_2sidesym_leftback3linearx : 2bs2sslb3_block (8+2 images)
 4back3sym_2sidesym_leftback3linearx : 4bs2sslb3_block (12+2 images)
-4back3sym_2sidesym_leftback3linearx_sa : 4bs2sslb3_sa (12+2 images)
-4back3sym_3sidesym_leftback3linearx_sa : 4bs3sslb3_sa (14+2 images)
-2backsym_2sidesym_leftback3linearx_sa : 2bs2sslb3_sa (8+2 images for GPU)
+4back3sym_2sidesym_leftback3linearx_sa : 4bs2sslb3_sa (12+2 images), 4bs2sslb3_sa_apriltag
+4back3sym_3sidesym_leftback3linearx_sa : 4bs3sslb3_sa (14+2 images), 4bs3sslb3_sa_apriltag
+2backsym_2sidesym_leftback3linearx_sa : 2bs2sslb3_sa (8+2 images for GPU), 2bs2sslb3_sa_apriltag
 
 """
 experiment_tag = "2backsym_2sidesym_leftback3linearx_sa"
@@ -21,7 +21,7 @@ pose_data = exp_config.get_config(experiment_tag)
 
 
 # Running the arm to get pictures in assigned poses
-images_saving_name = "2bs2sslb3_sa"
+images_saving_name = "2bs2sslb3_sa_apriltag"
 saving_dir = f"arm_captured_images/{images_saving_name}"
 if not os.path.exists(saving_dir):
     os.makedirs(saving_dir)
