e1e03cb89f2953324a50aea5ef82950f43fd5556
Tom <hz20020801@gmail.com>
2024-03-15 09:00:53 -0400

    update

--- generate_images.py
+++ generate_images.py
@@ -3,7 +3,7 @@ from utils.motion_cam_images import generate_images
 from configs.experiments_data_config import ArmDustrExpData
 
 exp_config = ArmDustrExpData()
-exp_name = "8obj_divangs"
+exp_name = "2obj_divangs"
 
 pose_data = exp_config.get_obs_config(exp_name)
 web_cam_idx = 2
